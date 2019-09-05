3c69c44c6a959a85dd99b836aa7b5212799b9a15
JEND-CODES <54572997+JEND-CODES@users.noreply.github.com>
2019-09-05 13:06:26 +0200

    Add files via upload

---
+++ content/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    //Materialize animations
+    $('.sidenav').sidenav();
+    $('.fixed-action-btn').floatingActionButton();
+    $('.dropdown-trigger').dropdown();
+    $('.scrollspy').scrollSpy();
+    $('.tabs').tabs();
+    
+    //To hide comments on chapters
+    $( "#ghost2" ).hide();
+    $( "#ghost3" ).hide();
+   
+});
+
+$( "#ghost" ).click(function() {
+  $( "#ghost2" ).slideToggle( "slow", function() {
+    // Show max 5 comments
+  });
+    $( "#ghost1" ).toggle( "slow", function() {
+    // Show All comments
+  });
+    $( "#ghost3" ).toggle(function() {
+    // Change message button
+  });
+    $( "#ghost4" ).toggle(function() {
+    // Change message button
+  });
+});
+
+
+ 
+
